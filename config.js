7a8480a2595b67a5a4df5d96b6c8d507d56fba0c
Douglas Brandão <38235074+douglasbrandao21@users.noreply.github.com>
2019-10-06 14:54:42 -0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: "service_account",
+  project_id: "pokeagent-ykpivf",
+  private_key_id: "f903a5f708478cd3932f4beb627868a37e402b66",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDkLhD4VfclRy82\n+gXGEDtXIDyyf2VqZntCwXUcVubPV0rmHDMnvg70GpE/1kzoOJwcz9mFcNld+fXC\nr8xNhULR3yLkgvhJq21gdAqdr6u0zeVWfr8TzwjU+MUynHXpXNNcy0/LMm9hab9b\njcQcXc2Ze9ITxSYbOjHe5DchC5L6fA6Mjx50ieTDpnsMi557GNg4TDKc5H1k14El\nW3MXZqWBmXnwiiQ/Re9DPfSXdy/hY4GOxISBf1r93fKXSvsANiabJGf6UwNKTvHc\nD9TE1HrA0AkoCJV48kA/2lCWcD5waOJZUPVdmsYgvhs7fto0Djh4t908JLM/cUsY\nM9xEnhPlAgMBAAECggEAWRdld9j01QDMvg3z+Cf5S2TiXh1gxyD5apCnfaeumfEu\n5NbI4HEUXJoM8VxmjdsglR1QhIoRzOAeZWQHhJMOCmyxroA2mmkNHTZhuLzQ8Zjc\nvR9i0sSo4aK6OC8f2LxuKFKXtrJcC7Biz0f0dNDpSE6wsRhubv4TX8BCiCwBOLw/\no6a95vTmd1BkJzU3rqaV8jZ/MDDCXgoxXbPOPl3AhwY2Fp7a/a1/PJjywAPMYp6t\nVuhK17wyXe1pjmBswA8P9Qy0y6scGZSgsAZJaNqQDlYN++U/ygFKWkTAFtvh1TVi\ngtz5VD0AbQUS9LUJBfa0psSmQpLkeWDq4UEe0BI0xwKBgQDyFEAXoGuijGI6Npfm\neQMDgHf4DNrMQngFX4gwUAyyXrd4Gz3x9qjcotzwopVdQAKe/81N5ei7LXzOlHpb\nkCeGiYomgESBlFX9RI8RTIKPVH+YACQLro4alzpaNQchQn5sR5NV6eICMt6GfmpF\nBstqYLVCkts/T9NrGrU2tKk30wKBgQDxTTNVbUn6pD87OOSMTglmPY9GFgDpk55F\ny2fxdJlngXpavqxMaPklHSSmXWnhQLWbxAxXHfn/dfDnyTqwbjaSs55joN1zs9/P\nu9WluGEJjXpfZfpw9PbrYcWhee1JVApyyQxXX16Jxdmy2CSEf9Nk06bFydXD8wEa\nGX9ObSAqZwKBgGKJA52aWrWLVuBDIIlbcHKYdDyvXtANrjThsdmYe9KO4zcsP4ah\n1BIa6Lu6MfoG+AooLTUsBBNMbKTZowRqJOyKR4VsaHYmHuo8Yc+0yN/2kQkZ5ZIr\ntwPz0nitZP2Qx0zjxYEO9kPW2OT9x9vY7Cucuasjg8K9h1ztPh3KY6apAoGAYIk9\nL0EQ5rhNH7lMHiGeOwwHxMdKyCepm0ycIgo+0R0IwUMlFVCt1D5Lik8268oCnfWZ\n5kR03tHldiWYWDtPk/NOO5eA4SgmMyp3d9xC3hp+yam3vvcRzeuIsGhiGQMJRxDN\n4iqiXMYNqTI1sdidzump/9kWr1UJrNJkjxCzXKMCgYEA4SzO/6VvbcrdNngy9vcZ\nFGqB/L4wp0I77sEao6eYtvEFJj6BBmI4HuMBP8Tk9wQ/K5Aurolq6xk0GftsJxLG\nkc9FaNM2CANtlfaLMoB5DNQ6x66nd9B9bsectOgm5bueOj6RzV10qFOd4ji6q0Vn\n4mPFehAeHHMXjITmCJPvh44=\n-----END PRIVATE KEY-----\n",
+  client_email: "dialogflow-pqhokf@pokeagent-ykpivf.iam.gserviceaccount.com",
+  client_id: "102986084927595888945",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-pqhokf%40pokeagent-ykpivf.iam.gserviceaccount.com"
+};
